a85238d27b3c279e77c6a3c2da0da2e4f7d210ef
Patrick-André Holzmann <patrickholzmann4@gmail.com>
2025-11-06 15:47:15 +0100

    Update test_basic_calculator.py

--- ml_math/tests/test_basic_calculator.py
+++ ml_math/tests/test_basic_calculator.py
@@ -4,120 +4,62 @@ from src.basic_calculator import BasicCalculator
 # python -m pytest
 
 class TestCalculator:
-
+    @classmethod
+    def setup_class(cls):
+        """
+        
+        """
+        cls.basic_calculator = BasicCalculator()
+        cls.input_data = [
+            (1, 2),
+            (-1, 2),
+            (-1, -2)
+        ]
+        cls.input_data_invalid = [
+            ("Invalid", 1),
+            ([1, 2], 3)
+        ]
+        cls.ref_funcs = {
+            "add": lambda x, y: x + y,
+            "subtract": lambda x, y: x - y,
+            "divide": lambda x, y: x / y,
+            "multiply": lambda x, y: x * y,
+            "exponential": lambda x, y: x ** y
+        }
     # -------------------------------------------------- ADD --------------------------------------------------
-    @pytest.mark.parametrize("a,b,expected", [
-        (1, 1, 2),
-        (-1, 1, 0),
-        (0, 0, 0)
+    @pytest.mark.parametrize("func_name", [
+        "add",
+        "subtract",
+        "subtract",
+        "divide",
+        "multiply",
+        "exponential"
     ])
-    def test_add(self, a, b, expected):
+    def test_basic_calculator_outputs(self, func_name):
         """
         
         """
-        assert BasicCalculator().add(a, b) == expected
+        func = getattr(self.basic_calculator, func_name)
+        ref_func = self.ref_funcs[func_name]
+        for arr in self.input_data:
+            output = func(*arr)
+            expected = ref_func(*arr)
+            assert output == expected
 
-    @pytest.mark.parametrize("a,b", [
-        ("Hello", 3),
-        ([1, 2], 5)
+    @pytest.mark.parametrize("func_name", [
+        "add",
+        "subtract",
+        "subtract",
+        "divide",
+        "multiply",
+        "exponential"
     ])
-    def test_add_invalid(self, a, b):
+    def test_basic_calculator_invalids(self, func_name):
         """
         
         """
-        with pytest.raises(ValueError):
-            BasicCalculator().add(a, b)
-
-    # -------------------------------------------------- SUBTRACT --------------------------------------------------
-    @pytest.mark.parametrize("a,b,expected", [
-        (2, 1, 1),
-        (-1, -1, 0),
-        (0, 0, 0)
-    ])
-    def test_subtract(self, a, b, expected):
-        """
-        
-        """
-        assert BasicCalculator().subtract(a, b) == expected
-
-    @pytest.mark.parametrize("a,b", [
-        ("Hello", 1),
-        ([1, 2], 5)
-    ])
-    def test_subtract_invalid(self, a, b):
-        """
-        
-        """
-        with pytest.raises(ValueError):
-            BasicCalculator().subtract(a, b)
-
-    # -------------------------------------------------- DIVIDE --------------------------------------------------
-    @pytest.mark.parametrize("a,b,expected", [
-        (1, 1, 1),
-        (5, 1, 5),
-        (0, 1, 0)
-    ])
-    def test_divide(self, a, b, expected):
-        """
-        
-        """
-        assert BasicCalculator().divide(a, b) == expected
-
-    @pytest.mark.parametrize("a,b", [
-        ("Hello", 1),
-        (1, 0),
-        ([1, 2], 1)
-    ])
-    def test_divide_invalid(self, a, b):
-        """
-        
-        """
-        with pytest.raises(ValueError):
-            BasicCalculator().divide(a, b)
-
-    # -------------------------------------------------- MULTIPLY --------------------------------------------------
-
-    @pytest.mark.parametrize("a,b,expected", [
-        (1, 1, 1),
-        (-5, 2, -10),
-        (-2, -2, 4)
-    ])
-    def test_multiply(self, a, b, expected):
-        """
-        
-        """
-        assert BasicCalculator().multiply(a, b) == expected
-    
-    @pytest.mark.parametrize("a,b", [
-        ("Hello", 1),
-        ([1, 2], 1)
-    ])
-    def test_multiply_invalid(self, a, b):
-        """
-        
-        """
-        with pytest.raises(ValueError):
-            BasicCalculator().divide(a, b)
-
-    # -------------------------------------------------- EXPONENTIAL --------------------------------------------------
-    @pytest.mark.parametrize("a,b,expected", [
-        (2, 3, 8),
-        (-2, 3, -8),
-        (-2, -3, -0.125)
-    ])
-    def test_exponential(self, a, b, expected):
-        """
-        
-        """
-        assert BasicCalculator().exponential(a, b) == expected
-
-    @pytest.mark.parametrize("a,b", [
-        ("Hello", 1),
-        ([1, 2], 1)
-    ])
-    def test_exponential_invalid(self, a, b):
-        """
-        
-        """
-        with pytest.raises(ValueError):
-            BasicCalculator().exponential(a, b)
+        func = getattr(self.basic_calculator, func_name)
+        ref_func = self.ref_funcs[func_name]
+        for arr in self.input_data_invalid:
+            with pytest.raises(ValueError):
+                func(*arr)
